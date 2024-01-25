6268d3319ae21fc20f1d945b3f55e3c8493265d6
Chuck476 <cdierbach@towson.edu>
2024-01-25 15:38:20 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+// The Import Statement in Java
+// import java.util.Scanner
+
+public class Main {
+  
+  public static void main(String[] args) {
+    // Scanner input = new Scanner(System.in);
+    // java.util.Scanner input = new java.util.Scanner(System.in);
+
+    System.out.println("What is your name? ");
+    String name = input.next();
+    System.out.println("Hi " + name);
+  }
+}
