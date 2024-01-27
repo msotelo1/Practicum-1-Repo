6998dc56e8dd71ab28ff8640873621ca21ad4c61
Chuck Dierbach <cdierbach@towson.edu>
2024-01-27 15:12:55 -0500

    Update main_solution.java

--- solution/main_solution.java
+++ solution/main_solution.java
@@ -1,1 +1,12 @@
+// Practicum 1 - (The Import Statement in Java) Solution
 
+public class Main {
+  
+  public static void main(String[] args) {
+    java.util.Scanner input = new java.util.Scanner(System.in);
+
+    System.out.println("What is your name? ");
+    String name = input.next();
+    System.out.println("Hi " + name);
+  }
+}
